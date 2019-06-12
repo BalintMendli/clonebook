bd6161584210c78c481613ac3f7942ec6dc8de5f
zenott <mblevel@gmail.com>
2019-06-12 19:14:09 +0200

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ApplicationRecord
   has_many :received_friend_requests, :class_name => 'FriendRequest', :foreign_key => 'receiver_id'
   has_many :sent_requests_friends, through: :sent_friend_requests, :source => :receiver
   has_many :received_requests_friends, through: :received_friend_requests, :source => :sender
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
