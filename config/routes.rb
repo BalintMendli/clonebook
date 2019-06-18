df498c270da6939711e04efca9113d4f634f59b4
zenott <mblevel@gmail.com>
2019-06-18 23:06:50 +0200

    Add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'users', to: 'users#index'
   get 'profile/:id', to: 'users#show', as: 'profile'
   get 'friends', to: 'friend_requests#friends'
