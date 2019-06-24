b0bed1485daedca1d01b98475008c35233cb645f
zenott <mblevel@gmail.com>
2019-06-24 23:26:30 +0200

    Remove skip_before_actions

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  skip_before_action :require_login, only: [:new, :create]
+  # skip_before_action :require_login, only: [:new, :create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
